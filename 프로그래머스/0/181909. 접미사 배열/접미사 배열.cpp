10295c61bd786fcf58f0663976925bf32beb5c09
tontong2 <baetongtong18@naver.com>
2026-08-29 00:49:18 +0900

    [level 0] Title: 접미사 배열, Time: 0.04 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.cpp
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.cpp
@@ -9,11 +9,7 @@ vector<string> solution(string my_string) {
     size_t size = my_string.size();
 
     for(size_t i=0; i<size; i++){
-        string temp;
-        for(size_t idx = i; idx<size; idx++){
-            temp += my_string[idx];
-        }
-        answer.push_back(temp);
+        answer.push_back(my_string.substr(i));
     }
     
     sort(answer.begin(), answer.end());
